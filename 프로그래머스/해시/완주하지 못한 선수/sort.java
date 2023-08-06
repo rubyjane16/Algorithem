b272a427ae7621433e0d6f63b6390c87c9d5af3d
백다은 <89911621+rubyjane16@users.noreply.github.com>
2023-08-06 22:19:09 +0900

    Update sort.java

--- 프로그래머스/해시/완주하지 못한 선수/sort.java
+++ 프로그래머스/해시/완주하지 못한 선수/sort.java
@@ -1,1 +1,22 @@
+import java.util.Arrays;
+class Solution {
+  
+     
+      
+    public String solution(String[] participant, String[] completion) {
+    // 1. 두 배열을 정렬한다 
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
+   //   2. 다를 때 까지 찾는다. 
+        int i=0;
+        for(; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+}
+      // 3. 여기까지 왔으면 마지막 주자가 완주하지 못한 선수다.
+            return participant[i];
+        
+    }
+}
