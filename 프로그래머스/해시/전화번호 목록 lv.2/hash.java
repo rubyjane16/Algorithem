5f7a33b31aa4642a9cb45fd893d8abf288ff1650
백다은 <89911621+rubyjane16@users.noreply.github.com>
2023-08-10 18:04:03 +0900

    Update hash.java

--- 프로그래머스/해시/전화번호 목록 lv.2/hash.java
+++ 프로그래머스/해시/전화번호 목록 lv.2/hash.java
@@ -13,7 +13,7 @@ class Solution {
        for(int i=0; i<phone_book.length; i++)
            for(int j=1; j<phone_book[i].length(); j++)//접두어의 길이를 정함
                //phone_book[i]의 길이 미만까지만 loop를 돌기때문에 자기 자신을 자연스럽게 걸러진다.
-               if(map.containsKey(phone_book[i].substring(0,j)))//처음부터 시작해서 j번째 값이 존재하느냐
+               if(map.containsKey(phone_book[i].substring(0,j)))//0부터 j까지니까 j=1로 시작
                    return false;
                    
            //3.여기까지 끝까지 없으면 접두어가 없는 것이다. 
