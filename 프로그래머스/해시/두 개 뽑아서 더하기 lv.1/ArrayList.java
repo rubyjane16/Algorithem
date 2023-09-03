afd4d44e30372ec083db38abfb94e476fa44e6d2
백다은 <89911621+rubyjane16@users.noreply.github.com>
2023-09-03 15:12:46 +0900

    Update ArrayList.java

--- 프로그래머스/해시/두 개 뽑아서 더하기 lv.1/ArrayList.java
+++ 프로그래머스/해시/두 개 뽑아서 더하기 lv.1/ArrayList.java
@@ -1,23 +1,21 @@
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
-   public int[] solution(int[] numbers){
-       ArrayList<Integer>arr=new ArrayList<>();
-    
-        for(int i=0; i<numbers.length-1;i++){
-            for(int j=i+1; j<numbers.length; j++){
-                int answer=numbers[i]+numbers[j];
-                if(arr.indexOf(answer)<0){//배열에 없는 수면
-                    arr.add(answer);
-                  
-                }
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> arr=new ArrayList<>(); //가변길이
+        
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=i+1; j<numbers.length; j++){//문자열 하나의 길이는 length()
+               int sum=numbers[i]+numbers[j];
+                if(arr.indexOf(sum)<0)////배열에 없는 수면
+                    arr.add(sum);                   
             }
+        } //가변길이 배열은 length가 아니고 size, get 으로 받는다.
+        int[] answer = new int [arr.size()];
+        for(int i=0; i<arr.size(); i++){
+            answer[i]=arr.get(i);
         }
-       
-      int[] answer=new int[arr.size()];
-       for(int i=0; i<arr.size(); i++){
-           answer[i]=arr.get(i);
-       }
+        
         Arrays.sort(answer);
         return answer;
     }
